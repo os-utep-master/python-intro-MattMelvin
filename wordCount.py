acabac625cdd69d6252cf7d21cf5fb7a7601cdab
Matthew Melvin <m98melvin@gmail.com>
2019-08-27 17:28:54 -0600

    Initial commit

---
+++ wordCount.py
@@ -0,0 +1,9 @@
+"""
+@author: mjmelvin
+Created August  27, 2019
+This program takes two text files, keeps track of the number of times each word occurs in the first file, and then
+writes the words and their totals two the second file
+"""
+
+if __name__ == '__main__':
+    # initial commit
