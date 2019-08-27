92464b664d55ddd5d7ef542b24c5b30d32c75772
Matthew Melvin <m98melvin@gmail.com>
2019-08-27 17:33:51 -0600

    Added functionality to open file and read text into word dictionary

--- wordCount.py
+++ wordCount.py
@@ -5,5 +5,18 @@ This program takes two text files, keeps track of the number of times each word 
 writes the words and their totals two the second file
 """
 
+import re
+import sys
+
+dictionary = {}
+pattern = r',.:;!?'
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
 if __name__ == '__main__':
-    # initial commit
+    with open(input_file) as f:
+        text = f.read()
+        text = re.sub(pattern, '', text)
+        text = text.lower()
+        words = text.split()
